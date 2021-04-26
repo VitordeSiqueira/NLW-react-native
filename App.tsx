8fa8bfc6383f9d46e525a847a596b54ad4f3f3cc
vitor dutra <vitor-siqueira2019@hotmail.com>
2021-04-25 23:05:54 -0300

    Ajustes finalizados

--- App.tsx
+++ App.tsx
@@ -1,57 +1,58 @@
 import React, { useEffect } from 'react';
-import {useFonts, Jost_400Regular, Jost_600SemiBold} from '@expo-google-fonts/jost'
-
-import * as Notifications from 'expo-notifications'
-
 import AppLoading from 'expo-app-loading';
-
-import Routes from './src/routes'
-
+import Routes from './src/routes';
+import * as Notifications from 'expo-notifications';
 import { PlantProps } from './src/libs/storage';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+ } from '@expo-google-fonts/jost';
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
+
+export default function App(){
+  const [ fontsLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
 
-/*   useEffect(() => {
-     const subscription = Notifications.addNotificationReceivedListener(
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    const subscription = Notifications.addNotificationReceivedListener(
       async notification => {
-        const data = notification.request.content.data.plant as PlantProps 
-        console.log(data)
+        await Notifications.cancelAllScheduledNotificationsAsync()
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+        return () => subscription.remove();
       }
     )
-    return () => subscription.remove(); 
-//CANCELAR NOTIFICAÇÃO
-        async function notifications(){
+
+    
+
+    /*async function notifications() {      
+      // cancelar notificações agendadas
       await Notifications.cancelAllScheduledNotificationsAsync();
+      // exibir notificações agendadas
       const data = await Notifications.getAllScheduledNotificationsAsync();
-       console.log("#### NOTIFICAÇÕES AGENDADAS ####")
-       console.log(data) 
+      console.log("######### NOTIFICAÇÕES AGENDADAS #########")
+      console.log(data);
     }
-    notifications();    
-  }, []) */
+    notifications();*/
 
+  },[])
 
-
-  if(!fontsLoaded){ //UTILIZADO PARA SEGURAR A TELA QUANDO AS FONTES AINDA NÃO FORAM CARREGADAS
-
+  if(!fontsLoaded)
     return <AppLoading />
 
-  }
-
   return (
-
-     <Routes/>
-
-  )
+    <Routes />
+  )  
 }
